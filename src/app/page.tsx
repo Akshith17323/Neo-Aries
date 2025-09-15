e4410ae79b2ee1bfc772906a5062a73e0fbe6b9a
Smanikanta21 <siraparapuabhinay21@gmail.com>
2025-09-15 21:06:00 +0530

    WIP: save local edits before pull

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,15 @@ export default function Landing() {
       </div>
       <div className=" hidden md:flex md:flex-row items-center gap-6 font-brand font-bold ">
         <a href="#" className="hover:text-red-900 transition-all duration-250 ease-in-out cursor-pointer">SHOP</a>
-        <a href="#" className="hover:text-red-900 transition-all duration-250 ease-in-out cursor-pointer">ABOUT</a>
-        <a href="#" className="hover:text-red-900 transition-all duration-250 ease-in-out cursor-pointer">CONTACT</a>
+        <a href="#" className="hover:text-red-900 transition-all duration-250 ease-in-out cursor-pointer">ABOU</a>
+        <a href="#" className="hover:text-red-900 transition-all duration-250 ease-in-out cursor-pointer">Contact</a>
       </div>
       <div>
         <ShoppingBag className="hover:text-red-900 transition-all duration-250 ease-in-out cursor-pointer"/>
       </div>
     </nav>
     <div style={{backgroundImage: 'url("https://preview--neo-aries-rebel-web.lovable.app/assets/hero-image-DFBSBDrk.jpg")',height:"100vh",opacity:"20"}} className="flex justify-center flex-col items-center object-cover">
-      <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-black font-brand mb-4 sm:mb-6 glitch leading-tight font-stretch-extra-expanded">NEO ARIES</h1>
+      <h1 className="text-4xl sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl font-black font-brand mb-4 sm:mb-6 glitch leading-tight">NEO ARIES</h1>
       <p className="text-2xl font-mono leading-3.5">REBELLIOUS STREETWEAR FOR THE UNDERGROUND CULTURE</p>
     </div>
     <div className="absolute inset-0 scan-line pointer-events-none"></div>
